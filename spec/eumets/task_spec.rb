36e1824e15f0543ac8e149c96bc7925ec4ae5cbe
dtan4 <dtanshi45@gmail.com>
2014-05-16 18:37:44 +0900

    Fix test fixtures

--- spec/eumets/task_spec.rb
+++ spec/eumets/task_spec.rb
@@ -3,7 +3,7 @@ require "time"
 
 module Eumets
   describe Task do
-    let(:params) do
+    let(:completed_params) do
       {
        id: "id",
        title: "title",
@@ -14,37 +14,40 @@ module Eumets
       }
     end
 
+    let(:incompleted_params) do
+      {
+       id: "id",
+       title: "title",
+       updated: "2014-05-18",
+       notes: "notes",
+       status: "needsAction",
+       due: "2014-05-18"
+      }
+    end
+
     describe "#initialize" do
       it "should set instance variables" do
-        task = described_class.new(params)
+        task = described_class.new(completed_params)
         expect(task.id).to eq "id"
         expect(task.title).to eq "title"
-        expect(task.updated).to eq Time.parse(params[:updated])
+        expect(task.updated).to eq Time.parse(completed_params[:updated])
         expect(task.notes).to eq "notes"
         expect(task.completed).to be_true
-        expect(task.due).to eq Time.parse(params[:due])
+        expect(task.due).to eq Time.parse(completed_params[:due])
       end
     end
 
     describe "#status_icon" do
       context "when the complete status is true" do
-        before do
-          params[:completed] = true
-        end
-
         it "should return ○" do
-          task = described_class.new(params)
+          task = described_class.new(completed_params)
           expect(task.status_icon).to eq "○"
         end
       end
 
       context "when the complete status is false" do
-        before do
-          params[:completed] = false
-        end
-
         it "should return ×" do
-          task = described_class.new(params)
+          task = described_class.new(incompleted_params)
           expect(task.status_icon).to eq "×"
         end
       end
