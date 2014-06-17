865b8051fc2173f49fc821913fe995f37f7cf0c4
dtan4 <dtanshi45@gmail.com>
2014-06-17 14:26:31 +0900

    Refactor specs

--- spec/eumets/cli_spec.rb
+++ spec/eumets/cli_spec.rb
@@ -6,8 +6,22 @@ module Eumets
       described_class.new
     end
 
+    shared_context "authorized" do
+      it "should not raise any Exception" do
+        allow_any_instance_of(described_class).to receive(:authorized?).and_return(true)
+        expect { action }.not_to raise_error
+      end
+    end
+
+    shared_context "unauthorized" do
+      it "should raise UnauthorizedException" do
+        allow_any_instance_of(described_class).to receive(:authorized?).and_return(false)
+        expect { action }.to raise_error UnauthorizedException
+      end
+    end
+
     describe "#list" do
-      let(:list) do
+      let(:action) do
         cli.list
       end
 
@@ -15,29 +29,12 @@ module Eumets
         allow(Eumets::Task).to receive(:find_by).and_return([])
       end
 
-      context "authorized" do
-        before do
-          allow_any_instance_of(described_class).to receive(:authorized?).and_return(true)
-        end
-
-        it "should not raise UnauthorizedException" do
-          expect { list }.not_to raise_error
-        end
-      end
-
-      context "unauthorized" do
-        before do
-          allow_any_instance_of(described_class).to receive(:authorized?).and_return(false)
-        end
-
-        it "should raise UnauthorizedException" do
-          expect { list }.to raise_error UnauthorizedException
-        end
-      end
+      include_context "authorized"
+      include_context "unauthorized"
     end
 
     describe "#add" do
-      let(:add) do
+      let(:action) do
         cli.add
       end
 
@@ -45,25 +42,8 @@ module Eumets
         allow(Eumets::Task).to receive(:add).and_return(nil)
       end
 
-      context "authorized" do
-        before do
-          allow_any_instance_of(described_class).to receive(:authorized?).and_return(true)
-        end
-
-        it "should not raise any Exception" do
-          expect { add }.not_to raise_error
-        end
-      end
-
-      context "unauthorized" do
-        before do
-          allow_any_instance_of(described_class).to receive(:authorized?).and_return(false)
-        end
-
-        it "should raise UnauthorizedException" do
-          expect { add }.to raise_error UnauthorizedException
-        end
-      end
+      include_context "authorized"
+      include_context "unauthorized"
     end
   end
 end
